f9054b8bbcd7afe1c8179d6e7a0a3e0f43fec3e2
Omar Hidalgo <omar_ehr@outlook.com>
2024-06-15 03:13:48 +0000

    ADD basic functions

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -7,11 +7,6 @@ export const Navbar = () => {
 			<Link to="/">
 				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
 			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
 		</nav>
 	);
 };
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,42 +1,26 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			apiURL: "https://www.swapi.tech/api",
+			people: [],
+			planets: [],
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
+			getPeople: async () => {
 				const store = getStore();
+					try {
+						const response = await fetch(`${store.apiURL}/people`,
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+						)
+						if(!response.ok){
+							throw new Error ("No se pudieron obtener los personajes");
+						}
+					} catch (error) {
+						console.log(error);
+					}
+			},
+			getPlanets: async () => {
 
-				//reset the global store
-				setStore({ demo: demo });
 			}
 		}
 	};
